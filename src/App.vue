c14fcf85bc89937d963dd5cb44db25af4157bbb3
Shi Han NG <shihanng@gmail.com>
2020-02-24 19:48:04 +0900

    Functional next button and answers

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <b-container class="bv-example-row">
       <b-row>
         <b-col sn="6">
-          <QuestionBox :currentQuestion="questions[index]" />
+          <QuestionBox v-if="questions.length" :currentQuestion="questions[index]" :next="next" />
         </b-col>
       </b-row>
     </b-container>
@@ -26,6 +26,11 @@ export default {
       questions: [],
       index: 0
     };
+  },
+  methods: {
+    next() {
+      this.index++;
+    }
   },
   mounted: function() {
     fetch("https://opentdb.com/api.php?amount=10&category=21&type=multiple", {
--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -4,11 +4,12 @@
       <template v-slot:lead>{{ currentQuestion.question }}</template>
 
       <hr class="my-4" />
-
-      <p>List of anwsers</p>
+      <b-list-group>
+        <b-list-group-item v-for="answer in answers" :key="answer">{{ answer }}</b-list-group-item>
+      </b-list-group>
 
       <b-button variant="primary" href="#">Submit</b-button>
-      <b-button variant="success" href="#">Next</b-button>
+      <b-button @click="next" variant="success" href="#">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
@@ -16,7 +17,25 @@
 <script>
 export default {
   props: {
-    currentQuestion: Object
+    currentQuestion: Object,
+    next: Function
+  },
+  computed: {
+    answers() {
+      let answers = [...this.currentQuestion.incorrect_answers];
+      answers.push(this.currentQuestion.correct_answer);
+      return answers;
+    }
   }
 };
 </script>
+
+<style scoped>
+.list-group {
+  margin-bottom: 15px;
+}
+
+.btn {
+  margin: 0 5px;
+}
+</style>
